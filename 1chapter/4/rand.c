abb8d22217288f44e55489ee61036986e09c5c47
AndSoAway <xyqhello@gmail.com>
2016-01-20 16:22:25 +0800

    add rand,c

--- 1chapter/4/rand.c
+++ 1chapter/4/rand.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-#define MAX 10000000
+#define MAX 	10000000
+#define COUNT 	1000000
 
 int a[MAX];
-
-void init();
-void swap(int i, int j);
-void generate();
+void swap(int *i, int *j);
+void generate(int max, int count,  int *arr);
 
 int main() {
-	init();	
+	generate(MAX, COUNT, a);	
 }
 
-void init() {
-	FILE* file = fopen("rand.txt", "wb");
-	for (int i = 0; i < MAX; i++)
+void generate(int max, int count, int *arr) {
+	FILE* file = fopen("data.txt", "wb");
+	for (int i = 0; i < max; i++)
 		a[i] = i;
-	for (int i = 0; i < MAX; i++) {
-		swap(i, randint(i, MAX - 1));
-		fprintf(file, "%d\n", a[i]);
+	
+	srand(time(NULL));
+	for (int i = 0; i < max; i++) {
+		int index = rand() % max;
+		if (index&2) {
+			swap(arr + i, arr + index);
+		} else {
+			swap(arr + i, arr + max - 1 - index);
+		}
+	}
+	
+	for (int i = 0; i < count; i++) {
+		fprintf(file, "%d\n", arr[i]);
 	}
 	fclose(file);
 }
 
-void swap(int i, int j) {
-	int tmp = a[i];
-	a[i] = a[j];
-	a[j] = a[i];
+void swap(int *i, int *j) {
+	int tmp = *i;
+	*i = *j;
+	*j = tmp;
 }
